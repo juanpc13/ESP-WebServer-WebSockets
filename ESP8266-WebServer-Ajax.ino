ada6aa09e42294401893244cd889e20f377b6d6c
Juan Pleitez <juanpc13lolol@gmail.com>
2018-02-02 00:00:11 -0600

    webserver api

--- ESP8266-WebServer-Ajax.ino
+++ ESP8266-WebServer-Ajax.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+#include "CSS.h"
+#include "HTML.h"
+#include "JavaScript.h"
 
+ESP8266WebServer server(80);
+const char* ssid = "........";
+const char* password = "........";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
+  WiFi.mode(WIFI_STA);
+  Serial.println("");
+  Serial.print("IP : ");
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);
+  server.on("/js", handleJS);
+  server.on("/data", handleJsonData);
+  server.begin();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  server.handleClient();
 }
+
+void handleRoot() {
+ String s = MAIN_HTML; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+
+void handleJS(){
+  String s = MAIN_JS; //Read JS contents
+  server.send(200, "text/javascript", s); //Send page
+}
+
+void handleJsonData(){
+  String s = "";
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  
+  root["time"] = millis();
+  root.printTo(s);
+  
+  server.send(200, "application/json", s); //Send json page
+}
+
